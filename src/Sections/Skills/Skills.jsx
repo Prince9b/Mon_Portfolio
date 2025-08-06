04ebd52a77075eaeb2d20e284fe505af1322d4be
Prince <natoyoumpriincergmail.com>
2025-08-06 21:48:38 +0100

    Fix Netlify deployment

--- src/Sections/Skills/Skills.jsx
+++ src/Sections/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SkillBar from '../common/Skillbar';
-import styles from '../common/Skillbar.module.css'
+import styles from '../common/SkillBar.module.css'
 
 const skillsLan = [
   { name: 'HTML / CSS / Javascript', level: 60 },
