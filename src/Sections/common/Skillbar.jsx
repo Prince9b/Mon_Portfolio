4922cbd55b5325508c195f3d3e87be8529d65cf7
PRINCE NATO <natoyoumpriince@gmail.com>
2025-09-22 17:20:29 +0000

    Django 1

--- src/Sections/common/Skillbar.jsx
+++ src/Sections/common/Skillbar.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import styles from './SkillBare.module.css';
 
-function SkillBar({ name, level }) {
+function SkillBar({ name}) {
   return (
     <div className={styles.skill}>
       <span className={styles.label}>{name}</span>
-      <div className={styles.progressBar}>
-        <div
-          className={styles.progressFill}
-          style={{ width: `${level}%` }}
-        ></div>
-      </div>
-      <span className={styles.percent}>{level}%</span>
     </div>
   );
 }
